feb075c15abec37494f2c7765aa30f0b3f0e61ad
Thambo <ett191@ftr.com>
2022-03-25 20:19:57 +0100

    Removed weathermap token

--- projects/weatherapp/weatherapp/settings.py
+++ projects/weatherapp/weatherapp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6g*oj=s8j%m4g1(e=4wogd3@jw!_w3bun^u=t%)a2n-!m%lawh'
+SECRET_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
